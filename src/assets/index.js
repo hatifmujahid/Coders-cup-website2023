a0095f1a4c33f6c0a4d993c5fee36dad293c858c
Wahaj Javed <84095994+WahajJaved20@users.noreply.github.com>
2023-09-26 22:21:38 +0500

    home-page-button-created

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,7 +1,8 @@
 import homeBackground from "./homeBG.png";
 import codersCupLogo from "./logo.png";
-
+import hawkingsLogo from "./hawkinsSchool.png"
 export {
     homeBackground,
-    codersCupLogo
+    codersCupLogo,
+    hawkingsLogo
 }
--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,11 +1,11 @@
-import {codersCupLogo, homeBackground} from "../assets"
+import {codersCupLogo, homeBackground, hawkingsLogo} from "../assets"
 import "./homepage.css"
 function HomePage(){
     return (
         <>
         // TODO: NAVBAR
         <div style={{fontFamily:"spotifyMedium"}}>navbar div</div>
-        <div classname="homeimg" style={{
+        <div className="homeimg" style={{
           backgroundImage: `url(${homeBackground})`,
           backgroundSize: "contain",
           backgroundPosition: "top 4em center",
@@ -14,8 +14,13 @@ function HomePage(){
     
         }}>
             <div className="flex flex-col justify-center items-center">
-                <img src={codersCupLogo} alt="logo" style={{width:"40%",height:"40%", marginTop:"-20px",position:"relative", zIndex:2}} />
-                <p class="text-lg font-normal md:text-lg sm:text-sm text-white lg:text-xl dark:text-white w-6/12 text-center " style={{fontFamily:"spotifyBold", fontSize: "150%"}}>Prepare to embark on an extraordinary coding journey as we announce that registrations for the Coder's Cup, presented by ACM NUCES, are now open. In a world where coding meets the upside down, join us if you dare!</p>
+                <img src={codersCupLogo} alt="logo" className=" sm:w-2/5 sm:h-2/5 w-2/5 h-2/5 mt-[-20px] relative z-2" />
+                <p className="font-normal md:text-l sm:text-small text-white lg:text-[1.5rem] dark:text-white w-6/12 text-center  " style={{fontFamily:"spotifyMedium"}}>Prepare to embark on an extraordinary coding journey as we announce that registrations for the Coder's Cup, presented by ACM NUCES, are now open. In a world where coding meets the upside down, join us if you dare!</p>
+                <div>
+                  <button id= "glow" style={{fontFamily:"spotifyMedium"}} className=" hover:scale-105 z-2 mt-16 bg-white text-black py-3 px-10 rounded-full text-xl relative hover:bg-black hover:text-red-600  transition duration-300"> Register Today
+                  </button>
+                  <img src={hawkingsLogo} alt="logo" className="relative mt-[-50%] w-[50%] ml-[80%] z-10" />
+                </div>
             </div>
         </div>
         </>
