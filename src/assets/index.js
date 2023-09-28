fb4dc668fc611df7605b6564b6e8828d4f2b3498
Wahaj Javed <84095994+WahajJaved20@users.noreply.github.com>
2023-09-28 23:10:25 +0500

    teams_done

--- src/assets/index.js
+++ src/assets/index.js
@@ -2,9 +2,25 @@ import homeBackground from "./homeBG.png";
 import codersCupLogo from "./logo.png";
 import hawkingsLogo from "./hawkinsSchool.png"
 import acmLogo from "./acmnuces.png";
+import mindflayer from "./mindflayer.png";
+import mindFlayerHeader from "./mindflayerHeader.png";
+import shadowkeeper from "./shadowkeeper.png";
+import shadowkeeperHeader from "./shadowkeeperHeader.png";
+import hawkingsHounds from "./hawkinshounds.jpg";
+import hawkingsHoundsHeader from "./hawkinsHoundsHeader.png";
+import veccnaValkerie from "./veccnavalkerie.jpg";
+import veccnaValkerieHeader from "./veccnavalkerieHeader.png";
 export {
+    mindFlayerHeader,
     homeBackground,
     codersCupLogo,
     hawkingsLogo,
-    acmLogo
+    acmLogo,
+    mindflayer,
+    shadowkeeper,
+    shadowkeeperHeader,
+    hawkingsHounds,
+    hawkingsHoundsHeader,
+    veccnaValkerie,
+    veccnaValkerieHeader
 }
---
+++ src/components/teamList.js
@@ -0,0 +1,27 @@
+import { mindflayer,mindFlayerHeader } from "../assets/"
+import { shadowkeeper,shadowkeeperHeader } from "../assets/"
+import { hawkingsHounds, hawkingsHoundsHeader } from "../assets/";
+import { veccnaValkerie, veccnaValkerieHeader } from "../assets/";
+const teamList = [
+    {
+        backgroundImage: mindflayer,
+        headerImage: mindFlayerHeader,
+        description: "A mysterious and enigmatic team, wield their coding skills like dark sorcery, leaving a trail of awe and curiosity in the Coder's Cup competition."
+    },
+    {
+        backgroundImage: shadowkeeper,
+        headerImage: shadowkeeperHeader,
+        description: "The ShadowKeepers, with their secretive approach to programming, lurk in the digital shadows, ready to unveil their formidable skills and strategies at the Coder's Cup."
+    },
+    {
+        backgroundImage: hawkingsHounds,
+        headerImage: hawkingsHoundsHeader,
+        description: "Hawkin's Hounds, a team known for their relentless pursuit of coding excellence, brings a touch of '80s nostalgia to the Coder's Cup with their passion for unraveling digital mysteries."
+    },
+    {
+        backgroundImage: veccnaValkerie,
+        headerImage: veccnaValkerieHeader,
+        description: "Veccna's Valkyries, a team of fierce and determined coders, draw inspiration from the legendary Veccna, showcasing their unwavering resolve and coding prowess in the Coder's Cup."
+    }
+]
+export default teamList;
