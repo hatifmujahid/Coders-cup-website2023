7853c4c141edafda370aa823d1f62e94c0f58bd2
hatifmujahid <hatifmujahid08@gmail.com>
2023-09-29 15:38:30 +0500

    latest changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Parallax, ParallaxLayer } from "@react-spring/parallax"
+import { Parallax, ParallaxLayer} from "@react-spring/parallax"
 import "./App.css"
 import HomePage from "./components/HomePage"
 import "./fonts/CircularSpotifyText-Black.otf"
@@ -12,34 +12,20 @@ import teamList from "./components/teamList"
 import teambackground from "./assets/teamBackground.jpg"
 import teamHeader from "./assets/teamHeader.png"
 import fastbg from "./assets/fastbg.png"
-import { useEffect, useRef } from "react"
+import { useEffect, useRef, useState } from "react"
 function App() {
   const parallax = useRef();
-
-    
-
-    
+  const [page, setPage] = useState(0)
+  useEffect(() => {
+    parallax.current.scrollTo(setPage((page)=>page+1))
+  }, [])
   return (
     <div >
       <Parallax ref={parallax} pages={7} style={{overflow:"auto", top: 0, left: 0}}>
-        <ParallaxLayer  offset={0} speed={0} >
+        <ParallaxLayer  offset={0} speed={0}>
           <HomePage />
         </ParallaxLayer>
-        <ParallaxLayer offset={2} speed={-0.7}>
-          <div className="flex flex-col justify-center items-center"
-              style={{
-                backgroundImage: `url(${fastbg})`,
-                backgroundSize: "cover",
-                position: "relative",
-                backgroundPosition: "center",
-                backgroundRepeat: "round",
-                height: "100%",
-                width: "100%",
-                
-            }}>
-          </div>
-        </ParallaxLayer>
-        <ParallaxLayer offset={1} speed={0.1}>
+        <ParallaxLayer offset={1} speed={1} >
           <div className="flex flex-col justify-center items-center"
             style={{
               backgroundImage: `url(${teambackground})`,
@@ -56,11 +42,10 @@ function App() {
         </ParallaxLayer>
         {teamList.map((team,index) => {
           return (
-            <ParallaxLayer offset={index + 2} speed={(index + 2) * (0.2/2)} style={{top:0, left:0,height:"100vh", width:"100vw",backgroundImage:team.backgroundImage}} >
+            <ParallaxLayer offset={index + 2} speed={(index + 2)} style={{top:0, left:0,height:"100vh", width:"100vw"}} >
               <TeamPage backgroundImageUrl={team.backgroundImage} teamNameImage={team.headerImage} description={team.description} />
             </ParallaxLayer>
           )
-
         })}
         <ParallaxLayer offset={8} speed={0} style={{top:0, left:0,height:"100vh", width:"100vw"}} >
 
