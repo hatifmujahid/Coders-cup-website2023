092d5f633f642e7fe1f4d336955d32a267098075
hatifmujahid <hatifmujahid08@gmail.com>
2023-10-01 01:27:20 +0500

    button change

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import { acmLogo } from '../assets';
 import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 function Navbar() {
     const [isNavbarOpen, setNavbarOpen] = useState(false);
 
@@ -44,22 +45,25 @@ function Navbar() {
                 >
                     <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg  md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-black md:dark:bg-black dark:border-gray-700">
                         <li>
-                            <a
-                                href="/"
-                                class="home-btn block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0  md:p-0 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-                                aria-current="page"
-                                
-                            >
-                                Home
-                            </a>
+                            <Link to={'/'}>
+                                <p
+                                    href="/"
+                                    class="home-btn block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0  md:p-0 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                                    aria-current="page"
+                                    
+                                >
+                                    Home
+                                </p>
+                            </Link>
                         </li>
                         <li>
-                            <a
-                                href="/register"
-                                class="register-btn block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0  md:p-0 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-                            >
-                                Register
-                            </a>
+                            <Link to={'/register'}>
+                                <p
+                                    class="register-btn block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0  md:p-0 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                                >
+                                    Register
+                                </p>
+                            </Link>
                         </li>
                     </ul>
                 </div>
