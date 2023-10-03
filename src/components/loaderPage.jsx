b10f424de1e410f1d3a430577c064df6abc6a961
hatifmujahid <hatifmujahid08@gmail.com>
2023-10-03 20:01:39 +0500

    loading screen works

---
+++ src/components/loaderPage.jsx
@@ -0,0 +1,7 @@
+export default function LoaderPage() {
+    return (
+        <div className="h-screen w-screen flex justify-center items-center">
+            <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-gray-900"></div>
+        </div>
+    )
+}
