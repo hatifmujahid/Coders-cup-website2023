f91408824a4d6a427be61dae29a12a8b5deb2ed0
Muhammad Hatif Mujahid <56687118+hatifmujahid@users.noreply.github.com>
2023-10-03 20:46:02 -0700

    trying aboutpage

--- src/components/aboutPage.jsx
+++ src/components/aboutPage.jsx
@@ -6,6 +6,128 @@ export default function AboutPage() {
     return (
         <>
             <Navbar />
+            <div className="flex flex-col items-center justify-center min-h-screen bg-black">
+                <div
+                    className="fixed top-0 left-0 w-full h-full bg-cover bg-center"
+                    style={{
+                    backgroundImage: `url('/bg.png')`,
+                    }}
+                >
+                    {/* About Us Section */}
+                    <div className='flex flex-col md:flex-row items-center justify-center m-5'>
+                        <div className="flex justify-center flex-col w-full md:w-1/3 p-5 bg-white">
+                            <img src="/aboutUs.png" alt="about Us" className="w-5/6 mx-auto" />
+                            <p className="text-white mt-4 p-6 font-bold tracking-wide bg-black">
+                            The Coders' Cup is an intra-FAST programming competition that
+                            stands as the epitome of where rising minds polish and develop
+                            their coding abilities while debugging their very own creations.
+                            Riddled with challenging questions and mind-boggling problems,
+                            Coders' Cup is one of its kind. With the extra motivation that
+                            comes with the theme, Coders' Cup has the perfect recipe to remain
+                            unparalleled.
+                            </p>
+                        </div>
+
+                        
+
+                        {/* Excom Section */}
+                        <div className="flex items-start w-2/3 p-10">
+                        <div className="container">
+                        <div className="row">
+                            <img src="/Sprite-0001.png" alt="about Us" className="w-5/6 mx-auto" />
+                        </div>
+                            <p className="text-white mb-10 font-bold tracking-wider bg-black text-3xl">
+                            EXECUTIVE COMMITEE '23-'24
+                            </p>
+
+                            <div className="row mx-10">
+
+                            <div className="row relative ">
+                                <div className="custom-bg-color absolute bottom-0 left-1/2 transform -translate-x-1/2 w-60 h-24 rounded-lg z-10 "></div>
+                                <p className="absolute bottom-5 right-0 text-white p-2 z-30">
+                                President
+                                </p>
+                                <p className="absolute bottom-0 right-0 text-white p-2 z-30 text-xl">
+                                Mr. Ahmed
+                                </p>
+                                <img
+                                src="/Ahmed.png"
+                                alt="Person"
+                                className="w-40 h-25 relative z-20  right-10"
+                                />
+                            </div>
+
+                            <div className="row relative">
+                            <div className="custom-bg-color absolute bottom-0 left-1/2 transform -translate-x-1/2 w-60 h-24 rounded-lg z-10"></div>
+                            <p className="absolute bottom-5 right-0 text-white p-2 z-30">
+                            Vice-President
+                            </p>
+                            <p className="absolute bottom-0 right-0 text-white p-2 z-30 text-xl">
+                            Hira rehman
+                            </p>
+                            <img
+                                src="/hira.png"
+                                alt="Person"
+                                className="w-40 h-25 relative z-20  right-10"
+                            />
+                            
+                            </div>
+                            </div>
+
+
+
+                        <div className="row">
+                        <div className="row relative">
+                            <div className="custom-bg-color absolute bottom-0 left-1/2 transform -translate-x-1/2 w-60 h-24 rounded-lg z-10"></div>
+                            <p className="absolute bottom-5 right-0 text-white p-2 z-30">
+                                General Sec.
+                            </p>
+                            <p className="absolute bottom-0 right-0 text-white p-2 z-30 text-xl">
+                                Hamad Sami
+                            </p>
+                            <img
+                                src="/hamd.png"
+                                alt="Person"
+                                className="w-40 h-25 relative z-20  right-10"
+                            />                  
+                            </div>
+                            <div className="row relative">
+                            <div className="custom-bg-color absolute bottom-0 left-1/2 transform -translate-x-1/2 w-60 h-24 rounded-lg z-10"></div>
+                            <p className="absolute bottom-5 right-0 text-white p-2 z-30">
+                                Treasurer
+                            </p>
+                            <p className="absolute bottom-0 right-0 text-white p-2 z-30 text-xl">
+                                Sarim Latif Khan
+                            </p>
+                            <img
+                                src="/sarim.png"
+                                alt="Person"
+                                className="w-40 h-25 relative z-20  right-10"
+                            />                  
+                            </div>
+                            <div className="row relative">
+                            <div className="custom-bg-color absolute bottom-0 left-1/2 transform -translate-x-1/2 w-60 h-24 rounded-lg z-10"></div>
+                            <p className="absolute bottom-5 right-0 text-white p-2 z-30">
+                                Event Admin
+                            </p>
+                            <p className="absolute bottom-0 right-0 text-white p-2 z-30 text-xl">
+                                Usman Nisar
+                            </p>
+                            <img
+                                src="/usman.png"
+                                alt="Person"
+                                className="w-40 h-25 relative z-20  right-10"
+                            />                  
+                            </div>
+
+
+                        </div>
+                        </div>
+
+                        </div>
+                    </div>
+                </div>
+                </div>
 
             <footer class="bg-transparent pt-3">
                 <div class="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
